069cce296720b51317388fc9959b1c0b7acf09e5
Jr Bytes <itamazor@gmail.com>
2021-07-07 20:58:31 -0300

    Incremented next auth with github.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import s from './styles.module.sass'
 
 export default function SignInButton() {
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={s.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Jr Bytes
+      {session.user.name}
       <FiX color="#737380" className={s.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={s.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with GitHub
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from 'next/app'
+import { Provider as NextAuthProvider } from 'next-auth/client'
+
 import Header from '../components/Header'
 
 import '../styles/global.sass'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
