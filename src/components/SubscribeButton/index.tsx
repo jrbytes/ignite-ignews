9026f9e7a334fdaa476f1fada5ba6d42c714b4a4
Jr Bytes <itamazor@gmail.com>
2021-07-08 19:34:41 -0300

    Added pay Stripe.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import s from './styles.module.sass'
 
 type SubscribeButtonProps = {
@@ -5,10 +8,33 @@ type SubscribeButtonProps = {
 }
 
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      console.log(err)
+      alert(err.message)
+    }
+  }
+
   return (
     <button
       type='button'
       className={s.subscribeButton}
+      onClick={handleSubscribe}
     >
       Inscreva-se
     </button>
